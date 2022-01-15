46fa7e6cd48c6aec7e0c01b5bc4e803416b40d78
amoungui <samoungui@gmail.com>
2022-01-15 04:53:52 +0000

    setting ui design

--- app.py
+++ app.py
@@ -4,17 +4,20 @@ from Core.Page.OrderPage import OrderPage as orderPage
 import string
 import random
 
+st.set_page_config(page_title="Yahoo financial data visualization", page_icon=":bar_chart:") # , layout="wide"
+
 def Homepage():
     st.session_state.runpage = main_page
     homepage = homePage(st.session_state.runpage)
-    #homepage.home_page()
     return homepage
 
 def Orderpage():
 	st.session_state.runpage = main_page
 	orderpage = orderPage(st.session_state.runpage)
-	#orderpage.order_page()
 	return orderpage
+ 
+def dashboard():
+    st.subheader('Dashboard')
  
 def main_page():
     btn1 = st.sidebar.button('Dashboard')
@@ -35,5 +38,12 @@ def main_page():
         st.session_state.runpage = main_page
         st.session_state.runpage()
 
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+	            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 if __name__ == '__main__':
     main_page()
