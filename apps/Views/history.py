e77d2790d07332bc1cf0118fcd43c62186436587
amoungui <samoungui@gmail.com>
2022-01-26 23:48:10 +0000

    history page 2

--- apps/Views/history.py
+++ apps/Views/history.py
@@ -9,22 +9,12 @@ connect.create_actiontable()
 #str(st.session_state.username)
 
 def app():
-    components.html(
-        """
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        """
-        ,height=3,
-    )    
+    st.title('Historic of Transactions')
     if 'auth' not in st.session_state:
-
-        st.title('Historic of Transactions')
         st.write('\n')
         st.info('Log in to view all your last transactions!')
     else:
-        st.title('historic of activities')
-        st.write('\n\n')
+        st.write('\n')
         st.subheader('user details')
         st.write('\n')        
         user = connect.get_user_by_username(str(st.session_state.username))
@@ -32,6 +22,9 @@ def app():
         # bootstrap 4 collapse example
         components.html(
             """
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>            
             <table class="table">
             <thead class="thead-dark">
                 <tr>
@@ -53,7 +46,7 @@ def app():
             </tbody>
             </table>
             """.format(user[0][1], user[0][2], user[0][3], user[0][4])
-            ,height=600,
+            ,height=10,
         )                
         st.subheader('Your historic of transaction')
         actions = connect.get_actions_by_id(user[0][0])
@@ -62,6 +55,9 @@ def app():
             # bootstrap 4 collapse example
             components.html(
                 """
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>                
                 <table class="table">
                 <thead class="thead-dark">
                     <tr>
